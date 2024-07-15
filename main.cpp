7b1624c594637838edb4d806e52d41fd4cb06dba
jun1-per <jun1per@tutamail.com>
2024-07-15 17:34:46 +0300

    add new features

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  seq
+//
+//  Created by Ricardo on 16/8/7.
+//  Copyright © 2016年 Ricardo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
